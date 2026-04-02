7be696d75b88bede800ba2f30fb56d79bae90ed7
ValachCornel <valach727@gmail.com>
2026-04-02 19:56:10 +0300

    Am adaugat un proiect WPF. Am adaugat afisarea evenimentelor in cadrul interfatei. Nu am eliminat proiectul consola deoarece il folosesc pentru a adauga noi element

---
+++ ScheduleGUI/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess;
+using Schedule_Manager;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScheduleGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IStocareData manager;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            manager = StocareFactory.GetManager();
+            IncarcaDate();
+        }
+
+        private void IncarcaDate()
+        {
+            List<ScheduleEvent> events = manager.ObtineEvenimente();
+            listaCarduri.ItemsSource = events;
+        }
+    }
+}
---
+++ ScheduleGUI/StocareFactory.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess;
+using System.Configuration;
+using System.IO;
+
+namespace ScheduleGUI
+{
+    public static class StocareFactory
+    {
+        private const string FORMAT_SALVARE = "FormatSalvare";
+        private const string NUME_FISIER = "NumeFisier";
+
+        public static IStocareData GetManager()
+        {
+            string formatSalvare = ConfigurationManager.AppSettings[FORMAT_SALVARE] ?? "";
+            string numeFisier = ConfigurationManager.AppSettings[NUME_FISIER] ?? "";
+
+            string directorCurent = Directory.GetCurrentDirectory();
+            string folderSolutie = Directory.GetParent(directorCurent)?.Parent?.Parent?.Parent?.FullName ?? "";
+
+            string caleCompletaFisier = Path.Combine(folderSolutie, "DataAccess", $"{numeFisier}.{formatSalvare}");
+
+            if (formatSalvare != null)
+            {
+                switch (formatSalvare)
+                {
+                    default:
+                    case "memorie":
+                        return new EventsManager();
+                    case "txt":
+                        return new EventsManagerText(caleCompletaFisier);
+                }
+            }
+
+            return null;
+        }
+    }
+}
